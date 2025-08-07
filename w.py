b82c9b70c8734f5ce2d274789acf667eb005ed37
Szymon L <szymonlema636@gmail.com>
2025-08-07 15:56:15 +0200

    Create w.py

---
+++ w.py
@@ -0,0 +1,11 @@
+w = [
+    "jak pobrać", "gdzie pobrać", "skąd pobrać", "jak zdobyć",
+    "gdzie zdobyć", "jak ściągnąć", "jak zainstalować", "pobranie aplikacji",
+    "jak mieć aplikację", "pobierz aplikację", "pobieranie aplikacji",
+    "skąd ściągnąć", "gdzie znajdę aplikację", "jak dostać aplikację",
+    "jak zainstalowac aplikacje", "jak pobrac aplikacje", "pobieranie apki",
+    "pobierz apke", "skąd apka", "jak apka", "dajcie apkę", "gdzie apka",
+    "czy mogę pobrać", "chcę pobrać", "aplikacja download", "download app",
+    "ściągnij apkę", "ściągam apkę", "gdzie się pobiera", "jak pobrać purevideo",
+    "gdzie jest aplikacja", "link do aplikacji", "gdzie jest apka", "jak mogę pobrać", "jak moge pobrac"
+]
